69e762aac2fa1950eb70bcd77a07bd4e937e37bb
duylongtd <tranlong280403@gmail.com>
2025-05-28 11:32:20 +0700

    first init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">hello world</h1>
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
