e3994e8c02371385ba412be3cf26429f80b5e9d0
Tran Dinh Duy Long <121693600+duylongtd@users.noreply.github.com>
2025-07-25 23:13:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0', // allow external access
-    port: 5173,
+    port: 5174,
     allowedHosts: ['warehouse-g2.online'], // prevent blocked request error
   },
 })
