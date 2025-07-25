db351f7e95abcf95921712b9e3de8a9ff2cdf419
duylongtd <tranlong280403@gmail.com>
2025-07-25 22:36:00 +0700

    fix: api 9999

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9999/api/v1", // Replace with your API base URL
+  baseURL: "http://localhost:8080/api/v1", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
